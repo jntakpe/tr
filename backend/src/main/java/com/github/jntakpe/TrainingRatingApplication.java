cda6a2bc78344c3cceea3bcf88b8adbded813f9e
jntakpe <jntakpe@gmail.com>
2016-03-19 15:12:23 +0100

    Backend init

---
+++ backend/src/main/java/com/github/jntakpe/TrainingRatingApplication.java
@@ -0,0 +1,17 @@
+package com.github.jntakpe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Training rating main class
+ *
+ * @author jntakpe
+ */
+@SpringBootApplication
+public class TrainingRatingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrainingRatingApplication.class, args);
+    }
+}
