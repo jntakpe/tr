b11a83e13c9c546d10dbec27a3f274bb6987fa3a
jntakpe <j.ntakpe@gmail.com>
2016-07-28 15:26:51 +0200

    Closes #1 : Intégration de Swagger

---
+++ backend/src/main/java/com/github/jntakpe/config/SwaggerConfig.java
@@ -0,0 +1,65 @@
+package com.github.jntakpe.config;
+
+import com.github.jntakpe.config.properties.ApiProperties;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.ResponseEntity;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
+
+import static springfox.documentation.builders.PathSelectors.regex;
+
+/**
+ * Configuration de Swagger
+ *
+ * @author jntakpe
+ */
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    public static final String DEFAULT_INCLUDE_PATTERN = "/api/.*";
+
+    @Autowired
+    private ApiProperties apiProperties;
+
+
+    @Bean
+    public Docket swaggerDocket() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(getApiInfo())
+                .forCodeGeneration(true)
+                .genericModelSubstitutes(ResponseEntity.class)
+                .directModelSubstitute(Instant.class, Date.class)
+                .directModelSubstitute(LocalDate.class, Date.class)
+                .directModelSubstitute(LocalDateTime.class, Date.class)
+                .select()
+                .paths(regex(DEFAULT_INCLUDE_PATTERN))
+                .build();
+
+    }
+
+    private ApiInfo getApiInfo() {
+        return new ApiInfo(apiProperties.getTitle(),
+                apiProperties.getDescription(),
+                apiProperties.getVersion(),
+                apiProperties.getTermsOfServiceUrl(),
+                getContact(),
+                apiProperties.getLicense(),
+                apiProperties.getLicenseUrl());
+    }
+
+    private Contact getContact() {
+        ApiProperties.Contact contactProperties = apiProperties.getContact();
+        return new Contact(contactProperties.getName(), contactProperties.getUrl(), contactProperties.getMail());
+    }
+}
---
+++ backend/src/main/java/com/github/jntakpe/config/properties/ApiProperties.java
@@ -0,0 +1,117 @@
+package com.github.jntakpe.config.properties;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+/**
+ * Proprités de l'API exposée par SwaggerÈ
+ *
+ * @author jntakpe
+ */
+@Component
+@ConfigurationProperties(prefix = "api")
+public class ApiProperties {
+
+    private String title;
+
+    private String description;
+
+    private String version;
+
+    private String termsOfServiceUrl;
+
+    private String license;
+
+    private String licenseUrl;
+
+    private Contact contact = new Contact();
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
+
+    public String getTermsOfServiceUrl() {
+        return termsOfServiceUrl;
+    }
+
+    public void setTermsOfServiceUrl(String termsOfServiceUrl) {
+        this.termsOfServiceUrl = termsOfServiceUrl;
+    }
+
+    public String getLicense() {
+        return license;
+    }
+
+    public void setLicense(String license) {
+        this.license = license;
+    }
+
+    public String getLicenseUrl() {
+        return licenseUrl;
+    }
+
+    public void setLicenseUrl(String licenseUrl) {
+        this.licenseUrl = licenseUrl;
+    }
+
+    public Contact getContact() {
+        return contact;
+    }
+
+    public void setContact(Contact contact) {
+        this.contact = contact;
+    }
+
+    public static class Contact {
+
+        private String name;
+
+        private String url;
+
+        private String mail;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getUrl() {
+            return url;
+        }
+
+        public void setUrl(String url) {
+            this.url = url;
+        }
+
+        public String getMail() {
+            return mail;
+        }
+
+        public void setMail(String mail) {
+            this.mail = mail;
+        }
+    }
+}
