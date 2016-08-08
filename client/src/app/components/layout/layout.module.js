fdd4dec1d39d89c7479fbf62b6ef253d7c74c20a
jntakpe <jntakpe@gmail.com>
2016-08-08 14:49:25 +0200

    #15 Clean du dossier client

--- client/src/app/components/layout/layout.module.js
+++
@@ -1,9 +0,0 @@
-(function () {
-    'use strict';
-
-    angular
-        .module('tr.layout', [
-            'tr.layout.header',
-            'tr.layout.sidenav'
-        ]);
-})();
--- client/src/app/index.constants.js
+++
@@ -1,9 +0,0 @@
-/* global moment:false */
-(function () {
-    'use strict';
-
-    angular
-        .module('tr')
-        .constant('moment', moment);
-
-})();
