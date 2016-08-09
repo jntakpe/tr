7ca51c2b863289bd24ad548b1194bf621f5e2fbf
jntakpe <jntakpe@gmail.com>
2016-08-09 10:38:01 +0200

    #15 Finally using angular-cli with webpack

--- client/src/app/footer.ts
+++
@@ -1,8 +0,0 @@
-import {Component} from "@angular/core";
-
-@Component({
-    selector: 'Footer',
-    template: require('./footer.html')
-})
-export class Footer {
-}
--- client/src/app/header.ts
+++
@@ -1,8 +0,0 @@
-import {Component} from "@angular/core";
-
-@Component({
-    selector: 'Header',
-    template: require('./header.html')
-})
-export class Header {
-}
