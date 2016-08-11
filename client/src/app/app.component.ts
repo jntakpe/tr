a1be6a7f9d1dfc43b99d7dcb7980735cc5ad35c4
jntakpe <j.ntakpe@gmail.com>
2016-08-11 18:38:01 +0200

    #21 Ajout d'un composant header

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,9 +1,11 @@
 import {Component} from "@angular/core";
+import {HeaderComponent} from "./shared/layout/header/header.component";
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  styleUrls: ['app.component.scss']
+  styleUrls: ['app.component.scss'],
+  directives: [HeaderComponent]
 })
 export class AppComponent {
   title = 'app works!';
---
+++ client/src/app/shared/layout/header/header.component.ts
@@ -0,0 +1,9 @@
+import {Component} from "@angular/core";
+
+@Component({
+  selector: 'headerbar',
+  template: require('./header.component.html')
+})
+export class HeaderComponent {
+
+}
