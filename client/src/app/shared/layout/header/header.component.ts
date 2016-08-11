cbf7faf04d6f7703c02e8f69fefcddd3df1fdf14
jntakpe <j.ntakpe@gmail.com>
2016-08-11 18:42:00 +0200

    #21 Ajout du composant topbar

--- client/src/app/shared/layout/header/header.component.ts
+++ client/src/app/shared/layout/header/header.component.ts
@@ -1,9 +1,17 @@
-import {Component} from "@angular/core";
+import {Component, OnInit} from "@angular/core";
+import {TopbarComponent} from "./topbar/topbar.component";
 
 @Component({
   selector: 'headerbar',
-  template: require('./header.component.html')
+  template: require('./header.component.html'),
+  directives: [TopbarComponent]
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
 
 }
---
+++ client/src/app/shared/layout/header/topbar/topbar.component.ts
@@ -0,0 +1,15 @@
+import {Component, OnInit} from "@angular/core";
+
+@Component({
+  selector: 'topbar',
+  template: require('./topbar.component.html')
+})
+export class TopbarComponent implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+}
