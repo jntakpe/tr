ac18e2c5d7505b781db9956af5d0bfb85192a8a8
jntakpe <j.ntakpe@gmail.com>
2016-08-12 18:30:04 +0200

    #21 Affichage de la route Home

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,10 +3,12 @@ import {NgModule} from "@angular/core";
 import {CommonModule} from "@angular/common";
 import {FormsModule} from "@angular/forms";
 import {AppComponent} from "./app.component";
+import {RouterModule} from "@angular/router";
+import {routes} from "./app.route";
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, CommonModule, FormsModule],
+  imports: [BrowserModule, CommonModule, FormsModule, RouterModule.forRoot(routes)],
   providers: [],
   entryComponents: [AppComponent],
   bootstrap: [AppComponent]
---
+++ client/src/app/home/home.component.ts
@@ -0,0 +1,17 @@
+import {Component, OnInit} from "@angular/core";
+
+@Component({
+  selector: 'home',
+  template: require('./home.component.html')
+})
+export class HomeComponent implements OnInit {
+
+  constructor() {
+
+  }
+
+  ngOnInit() {
+
+  }
+
+}
