0ff4d3b227052050cd1cc9d5e69fdf74e47f82f9
jntakpe <j.ntakpe@gmail.com>
2016-08-12 15:00:26 +0200

    #21 Ajout du composant navbar

--- client/src/app/shared/layout/header/header.component.ts
+++ client/src/app/shared/layout/header/header.component.ts
@@ -1,10 +1,11 @@
 import {Component, OnInit} from "@angular/core";
 import {TopbarComponent} from "./topbar/topbar.component";
+import {NavbarComponent} from "./navbar/navbar.component";
 
 @Component({
   selector: 'headerbar',
   template: require('./header.component.html'),
-  directives: [TopbarComponent]
+  directives: [TopbarComponent, NavbarComponent]
 })
 export class HeaderComponent implements OnInit {
 
---
+++ client/src/app/shared/layout/header/navbar/navbar.component.ts
@@ -0,0 +1,15 @@
+import {OnInit, Component} from "@angular/core";
+
+@Component({
+  selector: 'navbar',
+  template: require('./navbar.component.html')
+})
+export class NavbarComponent implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+}
