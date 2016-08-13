ae84d0dde3fb4ab4bd428a61e28cb90b24c083a8
jntakpe <jntakpe@gmail.com>
2016-08-13 11:28:46 +0200

    #15 ajout login component

--- client/src/app/app.route.ts
+++ client/src/app/app.route.ts
@@ -1,6 +1,8 @@
 import {Routes} from '@angular/router';
 import {HomeComponent} from './home/home.component';
+import {LoginComponent} from './login/login.component';
 
 export const routes: Routes = [
-  {path: '', component: HomeComponent}
+  {path: '', component: HomeComponent},
+  {path: 'login', component: LoginComponent}
 ];
