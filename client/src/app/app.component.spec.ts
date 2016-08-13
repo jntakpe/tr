858f7f6ddb91a6a3d2cd183cbca8635665166f10
jntakpe <jntakpe@gmail.com>
2016-08-13 10:57:05 +0200

    #15 rc5 tests

--- client/src/app/app.component.spec.ts
+++ client/src/app/app.component.spec.ts
@@ -1,20 +1,24 @@
-/* tslint:disable:no-unused-variable */
-
-import {addProviders, inject} from '@angular/core/testing';
 import {AppComponent} from './app.component';
+import {TestBed} from '@angular/core/testing/test_bed';
+import {async} from '@angular/core/testing/async';
 
 describe('App: Client', () => {
   beforeEach(() => {
-    addProviders([AppComponent]);
+    TestBed.configureTestingModule({declarations: [AppComponent]});
   });
 
-  it('should create the app',
-    inject([AppComponent], (app: AppComponent) => {
+  it('should create the app', async(() => {
+    TestBed.compileComponents().then(() => {
+      const app = TestBed.createComponent(AppComponent).componentInstance;
       expect(app).toBeTruthy();
-    }));
+    });
+  }));
 
-  it('should have as title \'app works!\'',
-    inject([AppComponent], (app: AppComponent) => {
+  it('should have as title \'app works!\'', async(() => {
+    TestBed.compileComponents().then(() => {
+      const app = TestBed.createComponent(AppComponent).componentInstance;
       expect(app.title).toEqual('app works!');
-    }));
+    });
+  }));
+
 });
