4bd8ba573ad35fe2cfaa60bd2968c58e1257346a
jntakpe <j.ntakpe@gmail.com>
2016-08-18 14:30:12 +0200

    chore

--- client/src/app/shared/alert.service.ts
+++ client/src/app/shared/alert.service.ts
@@ -1,5 +1,3 @@
-/// <reference path="../../../typings/globals/toastr/index.d.ts" />
-
 import {Injectable} from '@angular/core';
 import * as toastr from 'toastr';
 
@@ -11,22 +9,27 @@ export class AlertService {
   }
 
   info(message: string, title?: string) {
+    // noinspection TypeScriptUnresolvedFunction
     toastr.info(message, title);
   }
 
   success(message: string, title?: string) {
+    // noinspection TypeScriptUnresolvedFunction
     toastr.success(message, title);
   }
 
   error(message: string, title?: string) {
+    // noinspection TypeScriptUnresolvedFunction
     toastr.error(message, title);
   }
 
   warning(message: string, title?: string) {
+    // noinspection TypeScriptUnresolvedFunction
     toastr.warning(message, title);
   }
 
   defaultErrorMsg() {
+    // noinspection TypeScriptUnresolvedFunction
     toastr.error('Une erreur inconnue est survenue', 'Erreur non gérée');
   }
 }
--- client/typings/index.d.ts
+++
@@ -1,1 +0,0 @@
-/// <reference path="globals/toastr/index.d.ts" />
