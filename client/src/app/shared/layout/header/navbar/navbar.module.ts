45b86030eb3129decea71aafdec419c170e980f8
jntakpe <j.ntakpe@gmail.com>
2016-08-26 16:38:43 +0200

    #15 Clean menubar

--- client/src/app/shared/layout/header/navbar/navbar.module.ts
+++ client/src/app/shared/layout/header/navbar/navbar.module.ts
@@ -1,7 +1,9 @@
 import {NgModule} from '@angular/core';
 import {NavbarComponent} from './navbar.component';
+import {SharedModule} from '../../../shared.module';
 
 @NgModule({
+  imports: [SharedModule],
   declarations: [NavbarComponent],
   exports: [NavbarComponent]
 })
--- client/src/app/shared/layout/layout.module.ts
+++ client/src/app/shared/layout/layout.module.ts
@@ -3,13 +3,12 @@ import {LayoutComponent} from './layout.component';
 import {layoutRouting} from './layout.routing';
 import {HomeModule} from '../../home/home.module';
 import {HeaderModule} from './header/header.module';
-import {SharedModule} from '../shared.module';
 import {FooterModule} from './footer/footer.module';
 import {BreadcrumbsModule} from './breadcrumbs/breadcrumbs.module';
 
 @NgModule({
   declarations: [LayoutComponent],
-  imports: [SharedModule, HeaderModule, BreadcrumbsModule, FooterModule, HomeModule, layoutRouting],
+  imports: [HeaderModule, BreadcrumbsModule, FooterModule, HomeModule, layoutRouting],
   exports: [LayoutComponent]
 })
 export class LayoutModule {
