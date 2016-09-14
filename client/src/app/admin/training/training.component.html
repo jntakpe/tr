0d79ee7011dd2203eda5542b965b07ce6b7b242c
jntakpe <j.ntakpe@gmail.com>
2016-09-14 18:23:24 +0000

    #34 Initialisation du composant formation

---
+++ client/src/app/admin/training/training.component.html
@@ -0,0 +1,1 @@
+<h1>Formations</h1>
---
+++ client/src/app/admin/training/training.component.ts
@@ -0,0 +1,17 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'training-component',
+  template: require('./training.component.html')
+})
+export class TrainingComponent implements OnInit {
+
+  constructor() {
+
+  }
+
+  ngOnInit() {
+
+  }
+
+}
