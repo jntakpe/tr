76a7d298604e80d093bdd881b5585f2c3d74e884
jntakpe <j.ntakpe@gmail.com>
2016-09-15 17:24:37 +0000

    Chore

--- backend/src/main/java/com/github/jntakpe/config/UriConstants.java
+++ backend/src/main/java/com/github/jntakpe/config/UriConstants.java
@@ -15,7 +15,7 @@ public final class UriConstants {
 
     public static final String SESSIONS = API + "/sessions";
 
-    public static final String RATINGS = "/ratings";
+    public static final String RATINGS = API + "/ratings";
 
     public static final String RATINGS_BY_SESSION = SESSIONS + "/{sessionId}" + RATINGS;
 
